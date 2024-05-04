031cac92d0fc343610138063a168047b7071c86b
Jaggishah <jugdeepshah@gmail.com>
2024-05-04 13:03:02 -0700

    added nodejs

--- src/index.mjs
+++ src/index.mjs
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { request } from "express";
 
 const app = express();
 
@@ -8,6 +8,15 @@ app.get('/', (request, response) => {
     response.send("hello, World!!")
 })
 
+// app.get('/api/users/:id',(request,response) => {
+//     const data = request.params.id
+//     response.send({"data":data})
+// })
+
+// app.get('/api/users',(request,response) => {
+//     const data = request.query
+//     response.send({"data":data})
+// })
 
 
 app.listen(PORT , () => {
