08977889eadb698925596e7165c8f9e9033d3c73
Jaggishah <jugdeepshah@gmail.com>
2024-05-05 13:27:57 -0700

    added discord oatuh

--- src/index.mjs
+++ src/index.mjs
@@ -1,12 +1,39 @@
 import express, { request } from "express";
 import AppRouter from "./routes/index.mjs"
+import mongoose from "mongoose";
+import cookieParser from "cookie-parser";
+import session from "express-session";
+import passport from "passport";
+import MongoStore from "connect-mongo";
+import mongoose from "mongoose";
+
+
+mongoose
+	.connect("mongodb://localhost/express_tutorial")
+	.then(() => console.log("Connected to Database"))
+	.catch((err) => console.log(`Error: ${err}`));
 
 const app = express()
-app.use(express.json())
-// sesssion
-// cookies
-// validator
-// passport-local
+app.use(express.json());
+	app.use(cookieParser("helloworld"));
+	app.use(
+		session({
+			secret: "anson the dev",
+			saveUninitialized: true,
+			resave: false,
+			cookie: {
+				maxAge: 60000 * 60,
+			},
+			store: MongoStore.create({
+				client: mongoose.connection.getClient(),
+			}),
+		})
+	);
+
+	app.use(passport.initialize());
+	app.use(passport.session());
+
+    
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (request, response) => {
@@ -16,11 +43,7 @@ app.get('/', (request, response) => {
 app.use(AppRouter)
 
 
-// app.get('/api/users',(request,response) => {
-//     const data = request.query
-//     response.send({"data":data})
-// })
-// put,patch,delete
+
 
 app.listen(PORT , () => {
     console.log( `Running on ${PORT}`)
--- src/routes/index.mjs
+++ src/routes/index.mjs
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import localRouter from "./getById.mjs";
-
+import authRouter from "./autho.mjs";
 const appRouters = Router()
 
 appRouters.use(localRouter)
+appRouters.use(authRouter)
 
 
 export default appRouters;
