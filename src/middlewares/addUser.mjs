1cc8fa6155070515c54f069f234d55a4ae2c097b
Jaggishah <jugdeepshah@gmail.com>
2024-05-05 15:04:31 -0700

    added create user

--- src/middlewares/addUser.mjs
+++ src/middlewares/addUser.mjs
@@ -5,15 +5,23 @@ export const addingUser = async (request,response) => {
     const result = validationResult(request);
     console.log(result)
     if (!result.isEmpty()) return response.status(422).send(result.array())
+    var usersProjection = { 
+        _id: true,
+        _v:false
+    };    
     let { body } = request
+    let UserExist = await CreateUser.find(body,usersProjection).exec()
+    if (UserExist){
+        return response.status(409).send({"data":UserExist[0],"msg":"Data Exist!!"})
+    }
     let Create = new CreateUser(body)
     try{
         let data = await Create.save()
         if(data){
-            response.status(200).send(data)
+            return response.status(200).send({"data":data})
         }
     }catch(err){
-        response.status(500).send(err)
+        return response.status(500).send(err)
     }
     
 }
